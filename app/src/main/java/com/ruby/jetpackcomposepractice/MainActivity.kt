e9aa36a744bb56c2675161619b14205030a5713e
waiphyoaung <rubyland.myanmar.mg@gmail.com>
2024-02-20 03:28:58 +0630

    fix: add snack bar

--- app/src/main/java/com/ruby/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/ruby/jetpackcomposepractice/MainActivity.kt
@@ -7,12 +7,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -20,44 +28,60 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ruby.jetpackcomposepractice.composables.ButtonsEg
 import com.ruby.jetpackcomposepractice.ui.theme.JetpackComposePracticeTheme
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
-
 
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePracticeTheme {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = {
-                                Text("Jetpack Compose Cookbook")
-                            }
-                        )
-                    },
-
-                ){
-                    Box(modifier = Modifier.padding(it)) {
-                        MyApp()
-                    }
-                }
+                MyApp()
             }
         }
     }
 
     @Composable
     fun MyApp() {
+        val scope = rememberCoroutineScope()
+        val snackbarHostState = remember { SnackbarHostState() }
         val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = "home") {
-            composable("home") {
-                Home(navController = navController)
+        Scaffold(
+            snackbarHost = {
+                SnackbarHost(hostState = snackbarHostState)
+            },
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("Jetpack Compose Cookbook")
+                    }
+                )
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        snackbarHostState.showSnackbar("Snack Bar")
+                    }
+                }) {
+                    Icon(Icons.Filled.Add, contentDescription = "add")
+                }
             }
-            composable("buttons"){
-                ButtonsEg()
+
+            ) {
+            Box(modifier = Modifier.padding(it)) {
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") {
+                        Home(navController = navController)
+                    }
+                    composable("buttons") {
+                        ButtonsEg()
+                    }
+                }
             }
         }
+
     }
 
     @Composable
